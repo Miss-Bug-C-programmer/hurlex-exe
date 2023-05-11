18a20ad981fe40207410098866611ac0b6508a65
Miss-Bug-C-programmer <lxy_business@yeah.net>
2023-05-11 09:54:30 +0800

    gdt added

--- init/entry.c
+++ init/entry.c
@@ -1,13 +1,15 @@
-#include "types.h"
 #include "console.h"
+#include "debug.h"
+#include "gdt.h"
+
 
 int kern_entry()
 {
     init_debug();
-    
+    init_gdt();
+
     console_clear();
     printk_color(rc_black, rc_green, "Hello, OS kernel!\n");
-    panic("test");
 
     return 0;
 }
