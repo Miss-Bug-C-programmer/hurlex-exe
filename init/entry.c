7a7c1f02f1b5d75a5be6a4505e92d90b2f532e23
Miss-Bug-C-programmer <lxy_business@yeah.net>
2023-05-11 11:19:58 +0800

    added pmm module to travel mmap_entry and print out the range of the kernel address

--- init/entry.c
+++ init/entry.c
@@ -3,6 +3,7 @@
 #include "gdt.h"
 #include "idt.h"
 #include "timer.h"
+#include "pmm.h"
 
 int kern_entry()
 {
@@ -15,7 +16,12 @@ int kern_entry()
 
     init_timer(200);
 
-    asm volatile ("sti");
+    //asm volatile ("sti");
+    printk("kernel in memory start: 0x%08X\n", kern_start);
+    printk("kernel in memory end:   0x%08X\n", kern_end);
+    printk("kernel in memory used:   %d KB\n\n", (kern_end - kern_start) / 1024);
+
+    show_memory_map();
 
     return 0;
 }
